43f76c7261fdb104232e00b82a5cf804b91e7c8b
hym-dot <yumin6350@kbu.ac.kr>
2025-08-06 09:49:06 +0900

    Create index.js

---
+++ index.js
@@ -0,0 +1,19 @@
+const express = require("express");
+const app = express();
+const PORT = 3000;
+
+app.use(express.json());
+
+const boardRouter = require('./routes/board')
+app.use('/boards', boardRouter)
+
+
+
+
+app.get("/", (req, res) => {
+    res.send("Hello Express!");
+});
+
+app.listen(PORT, () => {
+    console.log("Server is running");
+});
